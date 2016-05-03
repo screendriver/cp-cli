0f6e082c1a459b6b81425b1208b450b163444d60
Christian Rackerseder <git@echooff.de>
2016-05-03 15:31:25 +0200

    First implementation

---
+++ src/cp-cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import fse from 'fs-extra';
+import yargs from 'yargs';
+
+const argv = yargs
+  .usage('Usage: $0 [-L] source target')
+  .demand(2, 2)
+  .boolean('d')
+  .alias('d', 'dereference')
+  .describe('d', 'Dereference symlinks')
+  .argv;
+
+const source = argv._[0];
+const target = argv._[1];
+const options = {
+  clobber: true,
+  dereference: argv.dereference,
+};
+
+fse.copy(source, target, options, error => {
+  if (error) {
+    console.error(error);
+  }
+});
---
+++ test/cp-cli-test.js
@@ -0,0 +1,10 @@
+import dirtyChai from 'dirty-chai';
+import chai, { expect } from 'chai';
+
+chai.use(dirtyChai);
+
+describe('cp-cli', () => {
+  it('should do anything', () => {
+    expect(true).to.be.true();
+  });
+});
