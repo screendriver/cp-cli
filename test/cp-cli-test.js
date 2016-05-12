a185aeddd88aa39ca465ce1b3fa742cc595bdca4
Christian Rackerseder <git@echooff.de>
2016-05-12 20:39:52 +0200

    Switched expect to should style

--- test/cp-cli-test.js
+++ test/cp-cli-test.js
@@ -1,8 +1,9 @@
+import chai from 'chai';
 import dirtyChai from 'dirty-chai';
 import fse from 'fs-extra';
 import shell from 'shelljs';
-import chai, { expect } from 'chai';
 
+const should = chai.should();
 chai.use(dirtyChai);
 
 describe('cp-cli', () => {
@@ -16,20 +17,20 @@ describe('cp-cli', () => {
 
   it('should print a help text when no arguments given', () => {
     const { stderr } = shell.exec('node bin/cp-cli');
-    expect(stderr).to.not.be.empty();
+    stderr.should.not.be.empty();
   });
 
   it('should print a help text when only one argument was given', () => {
     const { stderr } = shell.exec('node bin/cp-cli foo');
-    expect(stderr).to.not.be.empty();
+    stderr.should.not.be.empty();
   });
 
   it('should copy a source file to target dir', callback => {
     const { stderr } = shell.exec('node bin/cp-cli test/assets/foo.txt out/foo.txt');
-    expect(stderr).to.be.empty();
+    stderr.should.be.empty();
     fse.stat('out/foo.txt', (err, stats) => {
-      expect(err).to.be.null();
-      expect(stats.isFile()).to.be.true();
+      should.not.exist(err);
+      stats.isFile().should.be.true();
       callback();
     });
   });
@@ -39,14 +40,14 @@ describe('cp-cli', () => {
     shell.ln('-s', 'foo.txt', 'bar.txt');
     shell.cd('../..');
     const { stderr } = shell.exec('node bin/cp-cli -d test/assets out');
-    expect(stderr).to.be.empty();
+    stderr.should.be.empty();
     const promises = [];
     let promise = new Promise((resolve, reject) => {
       fse.stat('out/foo.txt', (err, stats) => {
         if (err) {
           reject(err);
         } else {
-          expect(stats.isFile()).to.be.true();
+          stats.isFile().should.be.true();
           resolve();
         }
       });
@@ -57,8 +58,8 @@ describe('cp-cli', () => {
         if (err) {
           reject(err);
         } else {
-          expect(stats.isFile()).to.be.true();
-          expect(stats.isSymbolicLink()).to.be.false();
+          stats.isFile().should.be.true();
+          stats.isSymbolicLink().should.be.false();
           resolve();
         }
       });
