9ef345394738f11d03c82cbdb530823cde900e41
Christian Rackerseder <git@echooff.de>
2017-08-12 16:20:21 +0200

    Enabled copying to a directory 📂
    Fixes #2

--- src/cp-cli.ts
+++ src/cp-cli.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import * as fs from 'fs';
 import * as fse from 'fs-extra';
+import * as path from 'path';
 import * as yargs from 'yargs';
 
 const argv = yargs
@@ -13,11 +15,15 @@ const argv = yargs
   .alias('h', 'help').argv;
 
 const source = argv._[0];
-const target = argv._[1];
+let target = argv._[1];
 const options: fse.CopyOptions = {
   dereference: argv.dereference,
   overwrite: true,
 };
+
+if (fse.pathExistsSync(target) && fs.lstatSync(target).isDirectory()) {
+  target = path.join(target, source);
+}
 
 fse.copy(source, target, options).catch((error: Error) => {
   if (error) {
