5b0651f14d54c5b37c32db7d2d904bce1dd998c0
Christian Rackerseder <git@echooff.de>
2018-10-15 19:45:34 +0200

    ✅ Switch from Jest to tape

--- test/cp-cli.test.ts
+++ test/cp-cli.test.ts
@@ -1,67 +1,90 @@
 import fse from 'fs-extra';
 import { platform } from 'os';
 import shell from 'shelljs';
+import test, { Test, TestCase } from 'tape';
 
-describe('cp-cli', () => {
-  afterEach(async () => {
+function withAfterEach(cb: (t: Test) => void): TestCase {
+  return async t => {
+    cb(t);
     await Promise.all([fse.remove('out'), fse.remove('test/assets/bar.txt')]);
-  });
+  };
+}
 
-  it('should print a help text when no arguments given', () => {
+test(
+  'print a help text when no arguments given',
+  withAfterEach(t => {
+    t.plan(1);
     const { stderr } = shell.exec('node bin/cp-cli');
-    expect(stderr).not.toEqual('');
-  });
+    t.notEqual(stderr, '');
+  }),
+);
 
-  it('should print a help text when only one argument was given', () => {
+test(
+  'print a help text when only one argument was given',
+  withAfterEach(t => {
+    t.plan(1);
     const { stderr } = shell.exec('node bin/cp-cli foo');
-    expect(stderr).not.toEqual('');
-  });
+    t.notEqual(stderr, '');
+  }),
+);
 
-  it('should copy a source file to target dir', done => {
+test(
+  'copy a source file to target dir',
+  withAfterEach(t => {
     const { stderr } = shell.exec(
       'node bin/cp-cli test/assets/foo.txt out/foo.txt',
     );
-    expect(stderr).toEqual('');
+    t.equal(stderr, '');
     fse.stat('out/foo.txt', (err, stats) => {
-      expect(err).toBeNull();
-      expect(stats.isFile()).toEqual(true);
-      done();
+      t.equal(err, null);
+      t.true(stats.isFile());
+      t.end();
     });
-  });
+  }),
+);
 
-  if (platform() !== 'win32') {
-    it('should dereference symlinks', async () => {
+if (platform() !== 'win32') {
+  test(
+    'dereference symlinks',
+    withAfterEach(async t => {
+      t.plan(4);
       shell.cd('test/assets');
       shell.ln('-s', 'foo.txt', 'bar.txt');
       shell.cd('../..');
       const { stderr } = shell.exec('node bin/cp-cli -d test/assets out');
-      expect(stderr).toEqual('');
+      t.equal(stderr, '');
       let stats = await fse.stat('out/foo.txt');
-      expect(stats.isFile()).toEqual(true);
+      t.true(stats.isFile());
       stats = await fse.stat('out/bar.txt');
-      expect(stats.isFile()).toEqual(true);
-      expect(stats.isSymbolicLink()).toEqual(false);
-    });
-  }
+      t.true(stats.isFile());
+      t.false(stats.isSymbolicLink());
+    }),
+  );
+}
 
-  it('should copy directory content', done => {
+test(
+  'copy directory content',
+  withAfterEach(t => {
     const { stderr } = shell.exec('node bin/cp-cli test/assets out');
-    expect(stderr).toEqual('');
+    t.equal(stderr, '');
     fse.stat('out/foo.txt', (err, stats) => {
-      expect(err).toBeNull();
-      expect(stats.isFile()).toEqual(true);
-      done();
+      t.equal(err, null);
+      t.true(stats.isFile());
+      t.end();
     });
-  });
+  }),
+);
 
-  it('should copy directory content when target directory does not exist', async done => {
+test(
+  'copy directory content when target directory does not exist',
+  withAfterEach(async t => {
     await fse.ensureDir('out');
     const { stderr } = shell.exec('node bin/cp-cli test/assets out');
-    expect(stderr).toEqual('');
+    t.equal(stderr, '');
     fse.stat('out/foo.txt', (err, stats) => {
-      expect(err).toBeNull();
-      expect(stats.isFile()).toEqual(true);
-      done();
+      t.equal(err, null);
+      t.true(stats.isFile());
+      t.end();
     });
-  });
-});
+  }),
+);
